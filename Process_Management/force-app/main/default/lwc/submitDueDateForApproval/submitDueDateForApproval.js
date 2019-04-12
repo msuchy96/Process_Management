2270171cd0f9ddccaf155830c7d8934d012de61f
Maciej.Suchocki <maciej.suchocki@enxoo.com>
2019-04-12 18:30:18 +0200

    PM21 approval process implementation to change due date

---
+++ Process_Management/force-app/main/default/lwc/submitDueDateForApproval/submitDueDateForApproval.js
@@ -0,0 +1,43 @@
+import { LightningElement, api, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import submitDueDateApproval from '@salesforce/apex/ManageApprovalProcess.submitDueDateApproval';
+
+export default class SubmitDueDateForApproval extends LightningElement {
+    @api recordId;
+
+    updateAndSubmitApproval() {
+        var inputCmp = this.template.querySelector(".inputCmp");
+        var value = inputCmp.value;
+
+        submitDueDateApproval({ jobId: this.recordId, dueDateToChange: value })
+        .then(result => {
+            if(result.isSuccess) {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: result.msg,
+                        variant: 'success',
+                    }),
+                );
+            } else {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: result.msg,
+                        variant: 'Error',
+                    }),
+                );
+            }
+        })
+        .catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: JSON.stringify(error),
+                    variant: 'Error',
+                }),
+            );
+        });
+    }
+
+}
